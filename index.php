29d10fe3ef95cd71381e0b2693f2b32117c30578
ZodexNL <zodexnl@gmail.com>
2023-05-18 00:50:24 +0200

    initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Intents;
+use Discord\WebSockets\Event;
+
+$discord = new Discord([
+    'token' => 'token_from_env',
+    'intents' => Intents::getDefaultIntents()
+    //      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
+]);
+
+$discord->on('ready', function (Discord $discord) {
+    echo "Bot is ready!", PHP_EOL;
+
+    // Listen for messages.
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+        echo "{$message->author->username}: {$message->content}", PHP_EOL;
+        // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+    });
+});
+
+$discord->run();

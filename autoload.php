58b98b74777d577e07083cfd7a1ea83ad7ac3ec3
ZodexNL <zodexnl@gmail.com>
2023-05-20 23:15:43 +0200

    improved autoloader

--- autoload.php
+++ autoload.php
@@ -3,11 +3,7 @@
 // Automaticly include all files
 spl_autoload_register(function ($classname) {
 
-    $baseDir = __DIR__ . '/src';
-
-    $classname = str_replace('\\', '/', $classname);
-
-    $file = $baseDir . '/' . $classname . '.php';
+    $file = $classname = str_replace('\\', '/', $classname) . '.php';
 
     if (file_exists($file)) {
         require_once $file;

f3678324144a3fb2dafc4a67fa52a95325aad639
ZodexNL <zodexnl@gmail.com>
2023-05-26 22:38:05 +0200

    created coordinates response

--- src/OpenWeater/GeoCoding/Responses/CoordinatesResponse.php
+++ src/OpenWeater/GeoCoding/Responses/CoordinatesResponse.php
@@ -5,27 +5,78 @@ namespace src\OpenWeater\GeoCoding\Responses;
 use stdClass;
 
 /**
- *
- * @package src\OpenWeater\GeoCoding\Responses
+ * The coordinate response
+ * 
+ * @property string $country    The zipcode
+ * @property string $name       The city name
+ * @property string $lat        The latitude from the city
+ * @property string $lon        The longitude from the city
+ * @property string $state      The longitude state the city
  */
 class CoordinatesResponse
 {
 
-    // TODO: check if array would be better response
+    private stdClass $obj;
 
     /**
      * Create and return a new CoordinatesResponse
-     * @param object $val
-     * @return stdClass
+     * @param stdClass $data
+     * @return void
      */
-    public function __construct(object $val)
+    public function __construct(stdClass $data)
     {
         $obj = new stdClass();
-        $obj->lat = $val->lat;
-        $obj->lon = $val->lon;
-        $obj->country = $val->country;
-        $obj->state = $val->state;
+        $obj->country = $data->country;
+        $obj->name = $data->name;
+        $obj->lat = $data->lat;
+        $obj->lon = $data->lon;
+        $obj->state = isset($data->state) ? $data->state : 'No state found';
 
-        return $obj;
+        $this->obj = $obj;
+    }
+
+    /**
+     * Get the country
+     * @return string 
+     */
+    public function country(): string
+    {
+        return $this->obj->country;
+    }
+
+    /**
+     * Get the name
+     * @return string 
+     */
+    public function name(): string
+    {
+        return $this->obj->name;
+    }
+
+    /**
+     * Get the lat
+     * @return string 
+     */
+    public function lat(): string
+    {
+        return $this->obj->lat;
+    }
+
+    /**
+     * Get the lon
+     * @return string 
+     */
+    public function lon(): string
+    {
+        return $this->obj->lon;
+    }
+
+    /**
+     * Get the state
+     * @return string 
+     */
+    public function state(): string
+    {
+        return $this->obj->state;
     }
 }

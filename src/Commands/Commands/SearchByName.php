24ea6f709d3f7d27cd1f41975560c5db49635495
ZodexNL <zodexnl@gmail.com>
2023-05-26 23:50:01 +0200

    name search command

---
+++ src/Commands/Commands/SearchByName.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace src\Commands\Commands;
+
+use Discord\Builders\MessageBuilder;
+use Discord\Discord;
+use Discord\Parts\Interactions\Command\Option;
+use Discord\Parts\Interactions\Interaction;
+use Dotenv\Dotenv;
+use src\Commands\Helpers\ResponseCommand;
+use src\Commands\Traits\GeoCodingEmbedsTrait;
+use src\OpenWeater\GeoCoding\GeoCoding;
+use src\OpenWeater\GeoCoding\Responses\Errors\Helpers\ErrorInterface;
+use src\OpenWeater\GeoCoding\Responses\NameResponse;
+
+class SearchByName implements ResponseCommand
+{
+    use GeoCodingEmbedsTrait;
+
+    public static function getName(): string
+    {
+        return 'searchbyname';
+    }
+
+    public static function getDescription(): string
+    {
+        return 'Get information by a city name';
+    }
+
+    public static function getOptions(Discord $discord): ?array
+    {
+        return [
+            new Option($discord, [
+                'name' => 'city',
+                'description' => 'The city to get information about',
+                'type' => 3,
+                'required' => true,
+            ])
+        ];
+    }
+
+    public static function getType(): int
+    {
+        return 1;
+    }
+
+    public static function getResponse(Interaction $interaction, Discord $discord): void
+    {
+        $dotEnv = Dotenv::createImmutable(__DIR__ . '/../../../')->load();
+
+        $city = $interaction->data->options['city']->value;
+
+        $weatherResponse = new GeoCoding($_ENV['WEATHER_API_TOKEN']);
+        $response = $weatherResponse->searchByName($city);
+        if ($response instanceof NameResponse) {
+            self::returnResponse($interaction, $discord, $response);
+        } else {
+            self::returnError($interaction, $discord, $response);
+        }
+    }
+
+    public static function returnResponse(Interaction $interaction, Discord $discord, mixed $response): void
+    {
+        $interaction->respondWithMessage(MessageBuilder::new()->addEmbed(self::searchByNameEmbed($discord, $response)));
+    }
+
+    public static function returnError(Interaction $interaction, Discord $discord, ErrorInterface $response): void
+    {
+        $interaction->respondWithMessage(MessageBuilder::new()->addEmbed(self::errorEmbed($discord, $response)));
+    }
+}

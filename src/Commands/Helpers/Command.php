63dad7bea7d19fcc6b3eee710a88cbe74154fb31
ZodexNL <zodexnl@gmail.com>
2023-05-26 23:52:26 +0200

    moved dockblocks to interface

--- src/Commands/Helpers/Command.php
+++ src/Commands/Helpers/Command.php
@@ -8,9 +8,36 @@ use src\OpenWeater\GeoCoding\Responses\Errors\Helpers\ErrorInterface;
 
 interface Command
 {
+    /**
+     * Get the name of the command
+     * @return string 
+     */
     public static function getName(): string;
+
+    /**
+     * Get the description of the command
+     * @return string 
+     */
     public static function getDescription(): string;
+
+    /**
+     * Get the options of the command
+     * @param Discord $discord 
+     * @return array|null 
+     */
     public static function getOptions(Discord $discord): array | null;
+
+    /**
+     * Get the type
+     * @return int 
+     */
     public static function getType(): int;
+
+    /**
+     * Return the response in discord
+     * @param Interaction $interaction 
+     * @param Discord $discord 
+     * @return void 
+     */
     public static function getResponse(Interaction $interaction, Discord $discord): void;
 }
